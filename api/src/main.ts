b012299a96b432b1749aab3b61b41de4b21c71f9
Đỗ Duy Minh Tuấn <80260231+dodumitu@users.noreply.github.com>
2022-11-14 19:20:00 +0700

    add search function

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,18 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, { cors: true });
-
-//   app.useGlobalPipes(new ValidationPipe());
-//   // const { httpAdapter } = app.get(HttpAdapterHost);
-//   // app.useGlobalFilters(new ExceptionLoggerFilter(httpAdapter));
-
-//   await app.listen(5400);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 

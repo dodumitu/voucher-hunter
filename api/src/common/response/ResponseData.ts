1240a409ec2021fe31a0d2b7b8483b6c510c229c
dodumitu <dodumitu@gmail.com>
2022-11-28 20:13:57 +0700

    add paging from dev-long

---
+++ api/src/common/response/ResponseData.ts
@@ -0,0 +1,22 @@
+import { Paging } from './Paging';
+
+export class ResponseData {
+  readonly status: number;
+  readonly message: string;
+  readonly data: any;
+  readonly paging?: Paging;
+  readonly code?: number;
+  constructor(
+    status: number,
+    data: any,
+    message?: string,
+    paging?: Paging,
+    code?: number,
+  ) {
+    this.status = status;
+    this.message = message || 'Success';
+    this.data = data;
+    this.paging = paging;
+    this.code = code;
+  }
+}
